2bc4ec4cb8bc1adf29c0759896096fff2d0c78a6
Christopher <79084040+seventi71@users.noreply.github.com>
2026-09-05 21:58:44 +0100

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,248 @@
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+class ChromeSearchProvider {
+    constructor(extension) {
+        this._extension = extension;
+        this.providers = {
+            'open-link': {
+                name: 'Open Link',
+                description: 'Open link in chrome',
+                icon: 'chrome',
+                getQuery: function (terms) {
+                  let q = terms.join(" ");
+                  return /^https?:\/\//.test(q) ? q : `http://${q}`;
+                }
+            },
+            'google': {
+                name: 'Search Google',
+                description: 'Search online with Google',
+                icon: 'google',
+                getQuery: function (terms) {
+                  return `https://www.google.com/search?q=${terms.join(" ")}`;
+                }
+            },
+            'maps': {
+                name: 'Search Maps',
+                description: 'Search online with Maps',
+                icon: 'chrome-mnhkaebcjjhencmpkapnbdaogjamfbcj-Default',
+                getQuery: function (terms) {
+                  return `https://maps.google.com/?q=${terms.join(" ")}`;
+                }
+            },
+            'gemini': {
+                name: 'Ask Gemini',
+                description: 'Ask Gemini a question online',
+                icon: 'chrome-akmpjlhfgienmcamdndncbdfjpniighc-Default',
+                getQuery: function (terms) {
+                  return `https://www.google.com/search?udm=50&aep=12&q=${terms.join(" ")}`;
+                }
+            },
+        };
+    }
+
+    /**
+     * The application of the provider.
+     *
+     * Applications will return a `Gio.AppInfo` representing themselves.
+     * Extensions will usually return `null`.
+     *
+     * @type {Gio.AppInfo}
+     */
+    get appInfo() {
+        return null;
+    }
+
+    /**
+     * Whether the provider offers detailed results.
+     *
+     * Applications will return `true` if they have a way to display more
+     * detailed or complete results. Extensions will usually return `false`.
+     *
+     * @type {boolean}
+     */
+    get canLaunchSearch() {
+        return false;
+    }
+
+    /**
+     * The unique ID of the provider.
+     *
+     * Applications will return their application ID. Extensions will usually
+     * return their UUID.
+     *
+     * @type {string}
+     */
+    get id() {
+        return this._extension.uuid;
+    }
+
+    /**
+     * Launch the search result.
+     *
+     * This method is called when a search provider result is activated.
+     *
+     * @param {string} result - The result identifier
+     * @param {string[]} terms - The search terms
+     */
+    activateResult(result, terms) {
+        const query = this.providers[result].getQuery(terms);
+
+        Gio.AppInfo.launch_default_for_uri(query, null);
+    }
+
+    /**
+     * Create a result object.
+     *
+     * This method is called to create an actor to represent a search result.
+     *
+     * Implementations may return any `Clutter.Actor` to serve as the display
+     * result, or `null` for the default implementation.
+     *
+     * @param {ResultMeta} meta - A result metadata object
+     * @returns {Clutter.Actor|null} An actor for the result
+     */
+    createResultObject(meta) {
+        console.debug(`createResultObject(${meta.id})`);
+
+        return null;
+    }
+
+    /**
+     * Get result metadata.
+     *
+     * This method is called to get a `ResultMeta` for each identifier.
+     *
+     * If @cancellable is triggered, this method should throw an error.
+     *
+     * @async
+     * @param {string[]} results - The result identifiers
+     * @param {Gio.Cancellable} cancellable - A cancellable for the operation
+     * @returns {Promise<ResultMeta[]>} A list of result metadata objects
+     */
+    getResultMetas(results, cancellable) {
+        const { scaleFactor } = St.ThemeContext.get_for_stage(global.stage);
+
+        return new Promise((resolve, reject) => {
+            const cancelledId = cancellable.connect(
+                () => reject(Error('Operation Cancelled')));
+
+            const resultMetas = [];
+
+            for (const identifier of results) {
+                const provider = this.providers[identifier];
+                const meta = {
+                    id: identifier,
+                    name: provider.name,
+                    description: provider.description,
+                    // clipboardText: 'Content for the clipboard',
+                    createIcon: size => {
+                        return new St.Icon({
+                            icon_name: provider.icon,
+                            width: size * scaleFactor,
+                            height: size * scaleFactor,
+                        });
+                    },
+                };
+
+                resultMetas.push(meta);
+            }
+
+            cancellable.disconnect(cancelledId);
+            if (!cancellable.is_cancelled())
+                resolve(resultMetas);
+        });
+    }
+
+    /**
+     * Initiate a new search.
+     *
+     * This method is called to start a new search and should return a list of
+     * unique identifiers for the results.
+     *
+     * If @cancellable is triggered, this method should throw an error.
+     *
+     * @async
+     * @param {string[]} terms - The search terms
+     * @param {Gio.Cancellable} cancellable - A cancellable for the operation
+     * @returns {Promise<string[]>} A list of result identifiers
+     */
+    getInitialResultSet(terms, cancellable) {
+      const identifiers = [];
+
+      identifiers.push('gemini');
+      identifiers.push('google');
+      identifiers.push('maps');
+      identifiers.push('open-link');
+
+      return new Promise((resolve, reject) => {
+        const cancelledId = cancellable.connect(
+          () => reject(Error('Search Cancelled')));
+
+        cancellable.disconnect(cancelledId);
+        if (!cancellable.is_cancelled())
+          resolve(identifiers);
+      });
+    }
+
+    /**
+     * Refine the current search.
+     *
+     * This method is called to refine the current search results with
+     * expanded terms and should return a subset of the original result set.
+     *
+     * Implementations may use this method to refine the search results more
+     * efficiently than running a new search, or simply pass the terms to the
+     * implementation of `getInitialResultSet()`.
+     *
+     * If @cancellable is triggered, this method should throw an error.
+     *
+     * @async
+     * @param {string[]} results - The original result set
+     * @param {string[]} terms - The search terms
+     * @param {Gio.Cancellable} cancellable - A cancellable for the operation
+     * @returns {Promise<string[]>}
+     */
+    getSubsearchResultSet(results, terms, cancellable) {
+        if (cancellable.is_cancelled())
+            throw Error('Search Cancelled');
+
+        return this.getInitialResultSet(terms, cancellable);
+    }
+
+    /**
+     * Filter the current search.
+     *
+     * This method is called to truncate the number of search results.
+     *
+     * Implementations may use their own criteria for discarding results, or
+     * simply return the first n-items.
+     *
+     * @param {string[]} results - The original result set
+     * @param {number} maxResults - The maximum amount of results
+     * @returns {string[]} The filtered results
+     */
+    filterResults(results, maxResults) {
+        console.debug(`filterResults([${results}], ${maxResults})`);
+
+        if (results.length <= maxResults)
+            return results;
+
+        return results.slice(0, maxResults);
+    }
+}
+
+export default class ChromeSearchProviderExtension extends Extension {
+    enable() {
+        this._provider = new ChromeSearchProvider(this);
+        Main.overview.searchController.addProvider(this._provider);
+    }
+
+    disable() {
+        Main.overview.searchController.removeProvider(this._provider);
+        this._provider = null;
+    }
+}
